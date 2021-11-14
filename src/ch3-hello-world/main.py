96423230c076d14493b6686c168ec51fd8eabebe
sarye.haddadi <sarye@live.fr>
2021-11-14 13:14:54 +0100

    End ch 3

---
+++ src/ch3-hello-world/main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from web.StartupApp import StartupApp
+
+app = StartupApp().create_app()
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
+
+
